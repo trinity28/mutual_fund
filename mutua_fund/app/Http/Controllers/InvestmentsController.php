0dd163e0864e200e6ab1bedf91d6ca9320c02100
lovely <lovelypandey93@gmail.com>
2017-07-18 15:30:10 +0530

    api call returning data successfully

--- mutua_fund/app/Http/Controllers/InvestmentsController.php
+++ mutua_fund/app/Http/Controllers/InvestmentsController.php
@@ -14,23 +14,37 @@ class InvestmentsController extends Controller
     public function index()
     {
         $client = new Client();
-
-        	
-        $res = $client->request('POST', 'https://mutualfundsnav.p.mashape.com/',[
-                       'headers' =>[
-                "X-Mashape-Key" => "rt510bTGgOmsh5232VZUOzDR31cbp1oPYGFjsn3IHB1ZEuZ1OL",
+  $headers = [
+     "X-Mashape-Key" => "rt510bTGgOmsh5232VZUOzDR31cbp1oPYGFjsn3IHB1ZEuZ1OL",
                 "Content-Type" => "application/json",
                 "Accept" => "application/json"
-              ],
-              'timeout' => 2000,
-                'http_errors' => false,
-              "{\"scodes\":[119018,100520,119528,120503,118533]}"]);
+];
 
-          try {
-              dd( $res);
-               
-            } catch (RequestException $e) {
-                var_dump($e->getResponse()->getBody()->getContent());
-            }
+$GetOrder = [
+   
+       
+        //"scodes"=>[119018,100520,119528,120503,118533]
+         "search"=>["franklin india bluechip"]
+    
+
+];
+
+$client = new client();
+$res = $client->post('https://mutualfundsnav.p.mashape.com/', [
+    'headers' => $headers, 
+    'json' => $GetOrder,
+]);
+        	
+      
+
+
+
+
+
+          echo $res->getStatusCode();
+        // "200"
+        //echo $res->getHeader('content-type');
+        // 'application/json; charset=utf8'
+        echo $res->getBody();
 }
 }
